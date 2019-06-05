4564393eeaae6c291ab69428c7a2877c320b35ae
waxime <aveline.maxime@gmail.com>
2019-06-05 14:51:22 +0200

    Add push notification

---
+++ README.md
@@ -0,0 +1,8 @@
+
+`$ npm init -y`
+`$ npm i -D nodemon json-server`
+`$ npm i express body-parser cors node-fetch web-push`
+`$ npx web-push generate-vapid-keys --json > vapid.json`
+
+1er terminal : `$ npm run start`
+2eme terminal : `$ npm run start:database`
--- index.js
+++ index.js
@@ -1,4 +1,30 @@
-const express = require('express"');
+const express = require('express');
 const bodyparser = require('body-parser');
 const cors = require('cors');
 const webpush = require('web-push');
+const path = require('path');
+const fetch = require('node-fetch');
+const vapid = require('./vapid');
+
+const app = express();
+const port = 8083;
+
+app.use(bodyparser);
+app.use(cors('*'));
+
+app.post('/subscribe', async (req, res) => {
+  const subscription = req.body;
+  subscription.id = subscription.endpoint;
+
+  await fetch('http://localhost:3000/clients', {
+    method: "POST",
+    headers : {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(subscription)
+  });
+
+  res.status(201).json();
+});
+
+app.listen(port, () => console.log('App listening on port ', port));
